5e08df9d122e5431dce5a592499761120e0c20b9
AnnaElis <44591488+AnnaElis@users.noreply.github.com>
2023-03-19 02:33:04 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function decrement () {
     <div className="App">
     <h1>{value}</h1>
     <h1>{likes}</h1>
-    <input type="text" value={value} />
+    <input type="text" 
+    value={value} 
+    onChange={event => setValue(event.target.value)}
+    />
    <button onClick={increment}>Increment</button>
    <button onClick={decrement}>Decrement</button>
   </div>
