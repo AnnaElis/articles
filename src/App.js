fd7bdc642fa6cc074746ed8624529cb7f38d5e8b
AnnaElis <44591488+AnnaElis@users.noreply.github.com>
2023-03-19 02:36:19 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function decrement () {
 }
   return (
     <div className="App">
-    <h1>{value}</h1>
     <h1>{likes}</h1>
+    <h1>{value}</h1>
     <input type="text" 
     value={value} 
     onChange={event => setValue(event.target.value)}
