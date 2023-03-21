bb73df85e689fb1080341cc88340c75d53a54720
AnnaElis <44591488+AnnaElis@users.noreply.github.com>
2023-03-21 15:46:32 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import React, { useEffect, useState } from 'react';
 import "./style.css";
 
 export default function App() {
-  const [likes, setLikes] = useState (0)
   const [value, setValue] = useState ('Текст в инпуте')
-function increment () {
-setLikes (value, likes + 1)
- }
-function decrement () {
- setLikes (value, likes + 1)
-}
+
   return (
     <div className="App">
-    <h1>{likes}</h1>
-    <h1>{value}</h1>
-    <input type="text" 
-    value={value} 
-    onChange={event => setValue(event.target.value)}
-    />
-   <button onClick={increment}>Increment</button>
-   <button onClick={decrement}>Decrement</button>
+    <Counter/>
   </div>
   );
 }
+export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,19 @@
 import React, { useEffect, useState } from 'react';
 const Counter = function () {
-  
+  const [count, setCount] = useState(0)
+  function increment () {
+    setCount (value, count + 1)
+     }
+    function decrement () {
+     setCount (value, count + 1)
+    }
+  return (
+    <div>
+    <h1>{count}</h1>
+    <button onClick={increment}>Increment</button>
+   <button onClick={decrement}>Decrement</button>
+    </div>
+  )
 }
+
+export default Counter;
