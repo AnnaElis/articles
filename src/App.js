e4c2420620f41d5fdff9e7846f389c41b1de7b01
AnnaElis <44591488+AnnaElis@users.noreply.github.com>
2023-03-21 17:20:42 +0300

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import Counter from "./components/Counter"
-import ClassCounter from "./components/Counter"
+import Counter from "./components/Counter";
+import ClassCounter from "./components/Counter";
+import PostItem from "./components/PostItem";
 import "./style.css";
+import "./styles/app.css";
 
 export default function App() {
   const [value, setValue] = useState ('Текст в инпуте')
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            Javascript - язык программирования.
-          </div>
-          </div>
-          <button>Удалить</button>
-      </div>
+  <PostItem/>
+  <PostItem/>
+  <PostItem/>
+  <PostItem/>
+  <PostItem/>
   </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const PostItem = () => {
+  return(
+
+  <div>
+    
+    <div className="post">
+    <div className="post__content">
+      <strong>1. Javascript</strong>
+      <div>
+        Javascript - язык программирования.
+      </div>
+      </div>
+      <button>Удалить</button>
+  </div>
+
+    </div>
+  )
+}
+
+
+export default PostItem;
